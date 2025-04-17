97b1cb575c3f093fe81e98daba9ff2a3edb6e205
Milan Godawatta <milanoshae@gmail.com>
2025-04-17 19:30:44 +0200

    more robust makefile, fixed norminette in the libft folder, added parsing folder and an example.c file to check whether the whole structure is corresponding together and working

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: mgodawat <mgodawat@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2025/04/17 18:47:24 by mgodawat          #+#    #+#              #
-#    Updated: 2025/04/17 19:21:44 by mgodawat         ###   ########.fr        #
+#    Updated: 2025/04/17 19:27:43 by mgodawat         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -41,7 +41,7 @@ LIBFT = $(LIBFT_DIR)/libft.a
 SRCS = main.c \
        $(SRC_DIR)/execution/echo.c \
        $(SRC_DIR)/execution/exec.c \
-    #    $(SRC_DIR)/parsing/
+       $(SRC_DIR)/parsing/example.c \
 
 # Object files
 OBJS = $(patsubst %.c,$(OBJ_DIR)/%.o,$(SRCS))
--- src/parsing/example.c
+++ src/parsing/example.c
@@ -1,33 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   minishell.h                                        :+:      :+:    :+:   */
+/*   example.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mgodawat <mgodawat@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/04/17 19:09:34 by mgodawat          #+#    #+#             */
-/*   Updated: 2025/04/17 19:26:46 by mgodawat         ###   ########.fr       */
+/*   Created: 2025/04/17 19:26:07 by mgodawat          #+#    #+#             */
+/*   Updated: 2025/04/17 19:26:20 by mgodawat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef MINISHELL_H
-# define MINISHELL_H
+#include "minishell.h"
 
-# include "../libft/includes/libft.h"
-# include <dirent.h>
-# include <fcntl.h>
-# include <signal.h>
-# include <stdio.h>
-# include <stdlib.h>
-# include <string.h>
-# include <sys/ioctl.h>
-# include <sys/stat.h>
-# include <sys/types.h>
-# include <sys/wait.h>
-# include <termios.h>
-# include <unistd.h>
-
-/* testing makefile */
-void	print_hello(void);
-
-#endif
+void	print_hello(void)
+{
+	printf("Hello, World!\n");
+}

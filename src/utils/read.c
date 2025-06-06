0d536fc04eae7165c213c0e132bcbf449a76e19e
DylanHasinandrianina <d.hasinandrianina@gmail.com>
2025-06-06 19:00:49 +0200

    fix signal with no command return value

--- src/utils/read.c
+++ src/utils/read.c
@@ -6,16 +6,12 @@
 /*   By: shasinan <shasinan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/23 21:55:23 by mgodawat          #+#    #+#             */
-/*   Updated: 2025/05/27 15:27:13 by shasinan         ###   ########.fr       */
+/*   Updated: 2025/06/06 18:55:38 by shasinan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-/*changes added :
-	-setup_signal_parent();
-	-check NULL before "if (command[0] == '\0'")
-	-check the global variable*/
 char	*read_cmd(t_context *ctx)
 {
 	char	*command;
@@ -26,13 +22,13 @@ char	*read_cmd(t_context *ctx)
 		command = readline(YELLOW PROMPT RESET);
 		if (!command)
 			return (NULL);
+		if (g_signal)
+		{
+			ctx->last_exit_code = 130;
+			g_signal = 0;
+		}
 		if (command[0] == '\0')
 		{
-			if (g_signal)
-			{
-				ctx->last_exit_code = 130;
-				g_signal = 0;
-			}
 			free(command);
 			continue ;
 		}
